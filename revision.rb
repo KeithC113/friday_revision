09942bf7b4dd4bae67c55b7dcefb97d1002afeb8
KeithC113 <campbell_ka@yahoo.co.uk>
2020-05-08 11:48:23 +0100

    intiial commit for revision session

---
+++ revision.rb
@@ -0,0 +1,12 @@
+# this method should loop over all friends to see if given friends name exists
+# if yes return true, if name not found then return false
+
+def is_friend?(all_friends, friend_name)
+  # loop over each friend in friend array
+  # get friend's name
+  # if friend's name == friend_name,
+  # return true
+  # at end of loop, if friend name not found
+  # return false
+
+end
